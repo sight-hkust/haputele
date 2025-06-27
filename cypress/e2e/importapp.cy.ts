ddddf656fc5109b94879ccff3ad1453d4ea23f25
caron <caronng5565@gmail.com>
2025-06-27 10:21:53 +0800

    return by auth

--- cypress/e2e/importapp.cy.ts
+++ cypress/e2e/importapp.cy.ts
@@ -1,37 +1,55 @@
 describe("Import App via API", () => {
-  let workspaceId = "";
-  const accessToken = Cypress.env("EXTERNAL_API_ACCESS_TOKEN"); // Load from .env
-  let tj_auth_token = "";
-  it("Authenticate via API and verify token", () => {
-    cy.visit("http://localhost:3080/login?redirectTo=/");
-    cy.request({
-      method: "POST",
-      url: "http://localhost:3080/api/authenticate",
-      body: {
-        email: "user@abc.com",
-        password: "Pass123=",
-        redirectTo: "/",
-      },
-    }).then((response) => {
-      expect(response.status).to.eq(201);
-			// cy.log('object in res', JSON.stringify(response.body));
-			cy.log("tj_token:", response.headers["set-cookie"][0].split(";")[0].split("=")[1]);
-      tj_auth_token = response.headers["set-cookie"][0].split(";")[0].split("=")[1];
-    });
-    cy.visit("http://localhost:3080/hapu-workspace");
-    cy.url().should("include", "/hapu-workspace");
+	let workspaceId = "";
+  let slug=""
+	let token = "";
+  let user="";
 
-    cy.log("get workspaces");
-    cy.request({
-      method: "GET",
-      url: "http://localhost:3080/api/ext/users",
-      headers: {
-        Authorization: `Basic ${accessToken}`,
-        "Content-Type": "application/json"
-      },
-    }).then((res) => {
-      expect(res.status).to.eq(200);
-      cy.log(JSON.stringify(res.body));
-    });//cant get shit,external still disabled
-  });
+	it("Authenticate via API and verify token", () => {
+		// cy.visit("http://localhost:3080/login?redirectTo=/");
+		cy.request({
+			method: "POST",
+			url: "http://localhost:3080/api/authenticate",
+			body: {
+				email: "user@abc.com",
+				password: "Pass123=",
+				redirectTo: "/",
+			},
+		}).then((response) => {
+			expect(response.status).to.eq(201);
+			// cy.log('object in res', JSON.stringify(response.body));
+      token = response.headers["set-cookie"][0].split(";")[0].split("=")[1];
+      workspaceId = response.body["current_organization_id"]
+      slug=response.body["current_organization_slug"]
+      user=response.body["id"]
+      cy.log(`token:${token}\n;wid:${workspaceId}\n;slug:${slug}\n;user:${user}`)
+
+		});
+		cy.visit("http://localhost:3080/hapu-workspace");
+		cy.url().should("include", "/hapu-workspace");
+
+    
+    cy.log("get some sessions")
+    // cy.request({
+    //   method:"GET",
+    //   url:"http://localhost:3080/api/session?appId&workspaceSlug=hapy-workspace",
+    // }).then((res)=>{
+    //   cy.log('res',res.body);
+    // })
+    // cy.on("uncaught:exception", (err, runnable) => {
+    //   return false;
+    // });
+
+    // cy.log("how")
+    //    cy.request({
+    //   method:"GET",
+    //   url:"http://localhost:3080/api/organizations?status=active",
+    // }).then((res)=>{
+    //   cy.log('res',res.body);
+    // })
+    // cy.on("uncaught:exception", (err, runnable) => {
+    //   return false;
+    // }); 
+	});
+
+
 });
